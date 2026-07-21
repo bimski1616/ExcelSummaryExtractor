8ced9e71b48e62d8ac652c500949aca2a5598775
bimski1616 <bimoshop244@gmail.com>
2026-07-21 09:52:02 +0700

    Create project_rules.py

---
+++ project_rules.py
@@ -0,0 +1,18 @@
+# =====================================================
+# Project Specific Rules
+#
+# number_index:
+# 1 = ambil angka pertama
+# 2 = ambil angka kedua
+# 3 = ambil angka ketiga
+# =====================================================
+
+PROJECT_RULES = {
+
+    "25-RNP-HWI-IOH-10-003": {
+
+        "Total Revenue": 2,
+
+    }
+
+}
