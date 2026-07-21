e79a7345db20e52e1742b3a32b09a2a474d19d36
bimski1616 <bimoshop244@gmail.com>
2026-07-21 10:07:04 +0700

    Refactor PROJECT_RULES structure and remove comments
    Removed project-specific rules comments and updated the structure of PROJECT_RULES.

--- project_rules.py
+++ project_rules.py
@@ -1,18 +1,14 @@
-# =====================================================
-# Project Specific Rules
-#
-# number_index:
-# 1 = ambil angka pertama
-# 2 = ambil angka kedua
-# 3 = ambil angka ketiga
-# =====================================================
-
 PROJECT_RULES = {
 
     "25-RNP-HWI-IOH-10-003": {
 
-        "Total Revenue": 3,
-        "Total Cost (Before Finance Cost)": 3,
+        "occurrence": {
+
+            "Total Revenue": 2,
+
+            "Total Cost (Before Finance Cost)": 2,
+
+        }
 
     }
 
